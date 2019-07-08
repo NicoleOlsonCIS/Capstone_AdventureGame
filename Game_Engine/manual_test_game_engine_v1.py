274911b9551d7fb7848350d574860440f5f177ad
Nicole Olson <nicoleolson@Nicoles-iMac.local>
2019-07-08 13:35:12 -0700

    Complete manual test, add pytest unit testing of constructors

--- Game_Engine/manual_test_game_engine_v1.py
+++ Game_Engine/manual_test_game_engine_v1.py
@@ -4,35 +4,43 @@
 # 1 user created (object)
 #
 # Room topology # 1:
-#    ____	 	  ____	
+#    ____         ____
 #   |r1  |       |r2  |
-#   |    |-------|    |    
+#   |    |-------|    |
 #   |____|       |____|
-#      |	 	   |
-#      |		   |
-#    __|_		  _|__
+#      |           |
+#      |           |
+#    __|_         _|__
 #   |r4  |       |r3  |
-#   |    |-------|    | 
+#   |    |-------|    |
 #   |____|       |____|
 #
 #
 #
 # Room topology # 2: (diagonal entrances/exits)
-#
 #  ____          ____
 # |r1  |        |r4  |
 # |    |--------|    |
-# |____|        |____|  
+# |____|        |____|
 #       \      /
 #        \____/           ____
 #        |r2  |          |r3  |
 #        |    |----------|    |
 #        |____|          |____|
 #
-# 
+#
 
 import game_engine_v1
 from game_engine_v1 import *
+
+# Experimenting with Enums
+# from enum import Enum
+
+#class Direction(Enum):
+#	n = 1
+#	ne = 2
+#	e = 3
+
 
 def runTopologyOne():
 	
@@ -53,7 +61,6 @@ def runTopologyOne():
 	user = User(game, "user1", "Room 1", False)
 
 	room1.setAdjacentRooms(game)
-
 	room2.setAdjacentRooms(game)
 	room3.setAdjacentRooms(game)
 	room4.setAdjacentRooms(game)
@@ -66,40 +73,16 @@ def runTopologyTwo():
 	game = Game()
 	
 	# has doors to the east and south-east
-	room1 = Room 
-	(
-		game, 
-		"Room 1", 
-		"Room 1 description", 
-		[None,  None, "Room 4", "Room 2", None, None, None, None]
-	)
+	room1 = Room (game, "Room 1", "Room 1 description", [None,  None, "Room 4", "Room 2", None, None, None, None])
 	
 	# has doors to the north west, north east and east
-	room2 = Room
-	(
-		game, 
-		"Room 2", 
-		"Room 2 description", 
-		[None, "Room 4", "Room 3", None, None, None, None, "Room 1"]
-	)
+	room2 = Room(game, "Room 2", "Room 2 description", [None, "Room 4", "Room 3", None, None, None, None, "Room 1"])
 
 	# has doors to the west
-	room3 = Room
-	(
-		game, 
-		"Room 3",
-		"Room 3 description", 
-		[None, None, None, None, None, None, "Room 2", None]
-	)
+	room3 = Room(game, "Room 3","Room 3 description", [None, None, None, None, None, None, "Room 2", None])
 
 	# has doors to the west and south-west
-	room4 = Room
-	(
-		game, 
-		"Room 4", 
-		"Room 4 description", 
-		[None, None, None, None, None, "Room 2", "Room 1", None]
-	)
+	room4 = Room(game, "Room 4", "Room 4 description", [None, None, None, None, None, "Room 2", "Room 1", None])
 
 	room1.setAdjacentRooms(game)
 	room2.setAdjacentRooms(game)
@@ -115,6 +98,10 @@ def main():
 
 	# run topologyOne
 	game = runTopologyOne()
+
+	print("Topology 1\n\n")
+	print("Testing valid moves: \n\n")
+
 	# valid moves in topology # 1
 	valid_moves = ["e", "s", "w", "n", "s", "e", "n", "w"]
 
@@ -128,6 +115,9 @@ def main():
 		else:
 			print("Invalid request")
 
+	print("\n\n")
+	print("Testing invalid moves (manually check print of 'invalid request' for each): \n\n")
+	
 	# invalid moves in topology # 1 (cannot do  from Room 1)
 	invalid_moves = ["n", "w", "ne", "nw", "se", "sw"]
 
@@ -141,5 +131,52 @@ def main():
 		else:
 			print("Invalid request")
 
+	print("\n\n")
+	print("Topology 2 (diagonal moves)\n\n")
+	print("Testing valid moves (manually check that each move occurs): \n\n")
+	# run topologyTwo
+	game = runTopologyTwo()
+
+	# valid moves in topology # 2
+	valid_moves = ["e", "sw", "nw", "se", "e", "w", "nw"]
+
+	for m in valid_moves:
+		game.user.printUser(game)
+		game.setIsValid()
+		direction = m
+		isValidRequest = game.checkIsValid("move_user", None, direction)
+		if(isValidRequest):
+			game.executeRequest("move_user", None, direction)
+		else:
+			print("Invalid request")
+
+	print("\n\nTesting invalid moves (manually check that 'invalid request' is printed): \n\n")
+	
+	# invalid moves in topology # 2 (from room 1)
+	invalid_moves = ["ne", "n", "nw", "w", "sw"]
+
+	for m in invalid_moves:
+		game.user.printUser(game)
+		game.setIsValid()
+		direction = m
+		isValidRequest = game.checkIsValid("move_user", None, direction)
+		if(isValidRequest):
+			game.executeRequest("move_user", None, direction)
+		else:
+			print("Invalid request")
+
 # run main
 main()
+
+
+
+
+
+# Enum Experimentation
+# exits = {
+#	Direction.n : "Room 1",
+#	Direction.ne : "Room 3"
+#}
+
+#for key in doors.keys():
+#	print(str(key) + " " + doors[key])
---
+++ Game_Engine/test_pytest.py
@@ -0,0 +1,48 @@
+import game_engine_v1
+from game_engine_v1 import *
+
+
+
+### --- Unit Testing of "Room" Class Constructor --- ###
+
+# Unit Test # 1 -- Target: Room Constructor & Attribute "name"
+def test_setRoomName():
+    game = Game()
+    room1 = Room (game, "Room 1", None, [None, None, None, None, None, None, None, None])
+
+    assert(room1.name == "Room 1")
+
+
+# Unit Test # 2 -- Target: Room Constructor & Attribute "description"
+def test_setRoomDescription():
+    game = Game()
+    room1 = Room (game, None, "Room 1 description", [None, None, None, None, None, None, None, None])
+
+    assert(room1.description == "Room 1 description")
+
+
+# Unit Test # 3 -- Target: Room Constructor & Attribute "adjacentRoomNames[]"
+def test_setRoomAdjacentNames():
+    game = Game()
+    room1 = Room(game, None, None, ["x", None, None, None, None, None, None, None])
+
+    assert(room1.adjacentRoomNames[0] == "x")
+
+
+
+### --- Unit Testing of User Class constructor --- ###
+
+# Unit Test #4 -- Target: User Constructor & Attribute "name"
+def test_setUserName():
+    game = Game()
+    user = User(game, "name", None, False)
+
+    assert(user.name == "name")
+
+# Unit Test #5 -- Target: User Constructor & Attribute "current_room" (at start)
+def test_setStartingRoom():
+    game = Game()
+    user = User(game, None, "Room 1", False)
+
+    assert(user.current_room == game.getRoom("Room 1"))
+
