e08e4fe6e8f2bf699a227c647278c9d936207694
Nicole Olson <nicoleolson@Nicoles-iMac.local>
2019-07-11 13:08:33 -0700

    Add 3D, time and user direction

--- Game_Engine/manual_test_game_engine_v1.py
+++ Game_Engine/manual_test_game_engine_v1.py
@@ -1,9 +1,9 @@
 # Manual Testing Game Engine Unit of Capstone Adventure Game Version 1 (Week 3)
 # 
-# 4 Rooms created (objects) 
+# 4 Places created (objects) 
 # 1 user created (object)
 #
-# Room topology # 1:
+# Place topology # 1:
 #    ____         ____
 #   |r1  |       |r2  |
 #   |    |-------|    |
@@ -17,7 +17,7 @@
 #
 #
 #
-# Room topology # 2: (diagonal entrances/exits)
+# Place topology # 2: (diagonal entrances/exits)
 #  ____          ____
 # |r1  |        |r4  |
 # |    |--------|    |
@@ -31,65 +31,61 @@
 #
 
 import game_engine_v1
+import action
 from game_engine_v1 import *
-
-# Experimenting with Enums
-# from enum import Enum
-
-#class Direction(Enum):
-#	n = 1
-#	ne = 2
-#	e = 3
-
+from action import *
 
 def runTopologyOne():
 	
-	game = Game()
+	game = Game(1,1)
+
+	descriptions = ["place during day", "place during night"]
 	
 	# has doors to the east and south
-	room1 = Room (game, "Room 1", "Room 1 description", [None,  None, "Room 2", None, "Room 3", None, None, None])
+	place1 = Place (game, "Place 1", descriptions, [None,  None, "Place 2", None, "Place 3", None, None, None, None, None])
 	
 	# has doors to the west and south
-	room2 = Room(game, "Room 2", "Room 2 description", [None, None, None, None, "Room 4",  None, "Room 1", None])
+	place2 = Place(game, "Place 2", descriptions, [None, None, None, None, "Place 4",  None, "Place 1", None, None, None])
 
 	# has doors to the north and west
-	room3 = Room(game, "Room 3", "Room 3 description", ["Room 1", None, None, None, None, None, "Room 4", None])
+	place3 = Place(game, "Place 3", descriptions, ["Place 1", None, None, None, None, None, "Place 4", None, None, None])
 
 	# has doors to the north and east
-	room4 = Room(game, "Room 4", "Room 4 description", ["Room 2", None, "Room 3", None, None, None, None, None])
+	place4 = Place(game, "Place 4", descriptions, ["Place 2", None, "Place 3", None, None, None, None, None, None, None])
 
-	user = User(game, "user1", "Room 1", False)
+	user = User(game, "user1", "Place 1", "n", False) # starting the user facing north
 
-	room1.setAdjacentRooms(game)
-	room2.setAdjacentRooms(game)
-	room3.setAdjacentRooms(game)
-	room4.setAdjacentRooms(game)
+	place1.setAdjacentPlaces(game)
+	place2.setAdjacentPlaces(game)
+	place3.setAdjacentPlaces(game)
+	place4.setAdjacentPlaces(game)
 
 	game.setUser(user)
 	return game
 
 def runTopologyTwo():
 	
-	game = Game()
+	game = Game(1,1)
+	descriptions = ["place during day", "place during night"]
 	
 	# has doors to the east and south-east
-	room1 = Room (game, "Room 1", "Room 1 description", [None,  None, "Room 4", "Room 2", None, None, None, None])
+	place1 = Place (game, "Place 1", descriptions, [None,  None, "Place 4", "Place 2", None, None, None, None, None, None])
 	
 	# has doors to the north west, north east and east
-	room2 = Room(game, "Room 2", "Room 2 description", [None, "Room 4", "Room 3", None, None, None, None, "Room 1"])
+	place2 = Place(game, "Place 2", descriptions, [None, "Place 4", "Place 3", None, None, None, None, "Place 1", None, None])
 
 	# has doors to the west
-	room3 = Room(game, "Room 3","Room 3 description", [None, None, None, None, None, None, "Room 2", None])
+	place3 = Place(game, "Place 3", descriptions, [None, None, None, None, None, None, "Place 2", None, None, None])
 
 	# has doors to the west and south-west
-	room4 = Room(game, "Room 4", "Room 4 description", [None, None, None, None, None, "Room 2", "Room 1", None])
+	place4 = Place(game, "Place 4", descriptions, [None, None, None, None, None, "Place 2", "Place 1", None, None, None])
 
-	room1.setAdjacentRooms(game)
-	room2.setAdjacentRooms(game)
-	room3.setAdjacentRooms(game)
-	room4.setAdjacentRooms(game)
+	place1.setAdjacentPlaces(game)
+	place2.setAdjacentPlaces(game)
+	place3.setAdjacentPlaces(game)
+	place4.setAdjacentPlaces(game)
 
-	user = User(game, "user1", "Room 1", False) # start in room1
+	user = User(game, "user1", "Place 1", "n", False) # start in Place1 facing north
 	game.setUser(user)
 	return game
 
@@ -98,6 +94,11 @@ def main():
 
 	# run topologyOne
 	game = runTopologyOne()
+	game.setIsValid()
+
+	# testing moving
+	action1 = Action()
+	action1.setVerb("move_user")
 
 	print("Topology 1\n\n")
 	print("Testing valid moves: \n\n")
@@ -106,77 +107,64 @@ def main():
 	valid_moves = ["e", "s", "w", "n", "s", "e", "n", "w"]
 
 	for m in valid_moves:
+		
 		game.user.printUser(game)
-		game.setIsValid()
-		direction = m
-		isValidRequest = game.checkIsValid("move_user", None, direction)
-		if(isValidRequest):
-			game.executeRequest("move_user", None, direction)
-		else:
-			print("Invalid request")
+
+		action1.setDirection(m)
+
+		# call game as though you are the parser
+		game.fromParserToGame(action1)
 
 	print("\n\n")
 	print("Testing invalid moves (manually check print of 'invalid request' for each): \n\n")
 	
-	# invalid moves in topology # 1 (cannot do  from Room 1)
+	# invalid moves in topology # 1 (cannot do  from Place 1)
 	invalid_moves = ["n", "w", "ne", "nw", "se", "sw"]
 
 	for m in invalid_moves:
 		game.user.printUser(game)
-		game.setIsValid()
-		direction = m
-		isValidRequest = game.checkIsValid("move_user", None, direction)
-		if(isValidRequest):
-			game.executeRequest("move_user", None, direction)
-		else:
-			print("Invalid request")
+
+		action1.setDirection(m)
+
+		# call game as though you are the parser
+		game.fromParserToGame(action1)
 
 	print("\n\n")
 	print("Topology 2 (diagonal moves)\n\n")
 	print("Testing valid moves (manually check that each move occurs): \n\n")
+
+
 	# run topologyTwo
 	game = runTopologyTwo()
+	game.setIsValid()
+
+	# testing moving
+	action2 = Action()
+	action2.setVerb("move_user")
 
 	# valid moves in topology # 2
 	valid_moves = ["e", "sw", "nw", "se", "e", "w", "nw"]
 
 	for m in valid_moves:
 		game.user.printUser(game)
-		game.setIsValid()
-		direction = m
-		isValidRequest = game.checkIsValid("move_user", None, direction)
-		if(isValidRequest):
-			game.executeRequest("move_user", None, direction)
-		else:
-			print("Invalid request")
+
+		action2.setDirection(m)
+
+		# call game as though you are the parser
+		game.fromParserToGame(action2)
 
 	print("\n\nTesting invalid moves (manually check that 'invalid request' is printed): \n\n")
 	
-	# invalid moves in topology # 2 (from room 1)
+	# invalid moves in topology # 2 (from Place 1)
 	invalid_moves = ["ne", "n", "nw", "w", "sw"]
 
 	for m in invalid_moves:
 		game.user.printUser(game)
-		game.setIsValid()
-		direction = m
-		isValidRequest = game.checkIsValid("move_user", None, direction)
-		if(isValidRequest):
-			game.executeRequest("move_user", None, direction)
-		else:
-			print("Invalid request")
+
+		action2.setDirection(m)
+
+		# call game as though you are the parser
+		game.fromParserToGame(action2)
 
 # run main
 main()
-
-
-
-
-
-# Enum Experimentation
-# exits = {
-#	Direction.n : "Room 1",
-#	Direction.ne : "Room 3"
-#}
-
-#for key in doors.keys():
-#	print(str(key) + " " + doors[key])
