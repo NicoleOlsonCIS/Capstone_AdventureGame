6a3d34f61df08812d93588a56f5320abad52a91e
hartmanc <cobylhartman@gmail.com>
2019-07-26 18:11:06 -0700

    Additional unit test for 'put down thing'

--- Parser/test_parser_drop.py
+++ Parser/test_parser_drop.py
@@ -73,6 +73,24 @@ def test_leave_out_thing():
     # Now assert there've been no errors, else print them
     assert not errors, "errors occurred:\n{}".format("\n".join(errors))
 
+def test_put_down_thing():
+
+    errors = []
+
+    action = parser.parseInput("put down thing")
+
+    # Replace individual assertions with conditions
+    if not action.verb == "drop":
+        errors.append("expected 'drop', got " + str(action.verb))
+    if not action.direction == None:
+        errors.append("expected 'None' type, got " + str(action.direction))
+    if not action.direct_obj == "thing":
+        errors.append("expected 'thing', got " + str(action.direct_obj))
+    # TODO: Indirect object - table?
+
+    # Now assert there've been no errors, else print them
+    assert not errors, "errors occurred:\n{}".format("\n".join(errors))
+
 # More complex phrases 
 def test_leave_thing_on_table():
 
