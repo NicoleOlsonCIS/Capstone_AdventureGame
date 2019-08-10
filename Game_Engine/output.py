cc656750a1c25f8520db57a0bd298c4578577430
Nicole Olson <nicoleolson@Nicoles-iMac.local>
2019-08-09 21:28:31 -0700

    speach color

--- Game_Engine/output.py
+++ Game_Engine/output.py
@@ -240,7 +240,7 @@ class Output(object):
                             while lcount < count:
                                 sys.stdout.write(sb3) # no newline
                                 fullPrint += sb3
-                                sys.stdout.write(u'\u001b[38;5;$157m') # change to text color
+                                sys.stdout.write(u'\u001b[38;5;$214m') # change to text color
                                 # get number of characters in the line
                                 line_length = len(lines[lcount])
                                 if line_length == 24:
@@ -288,7 +288,7 @@ class Output(object):
                             sys.stdout.write(sb3) # no newline
                             fullPrint += sb3
                             time.sleep(0.1)
-                            sys.stdout.write(u'\u001b[38;5;$157m') # change to text color
+                            sys.stdout.write(u'\u001b[38;5;$214m') # change to text color
                             sys.stdout.write(str_inside)
                             fullPrint += str_inside
                             time.sleep(0.1)
@@ -349,7 +349,7 @@ class Output(object):
             sys.stdout.write(u"\u001b[1000D")
             sys.stdout.write(u"\033[" + str(lineCount - 1) + "A") # up the line count
             code -= 1
-            time.sleep(0.04)
+            time.sleep(0.08)
         sys.stdout.write(u'\u001b[38;5;232m')
         sys.stdout.write(stringToFade)
         #reset
